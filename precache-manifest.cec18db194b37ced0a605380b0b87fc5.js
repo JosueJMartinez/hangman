63cb1b99bd805eb1f48fefd06215eb39123a71fd
JosueJMartinez <jmarti61@gmail.com>
2020-11-15 23:56:34 -0600

    Updates

---
+++ precache-manifest.cec18db194b37ced0a605380b0b87fc5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbca897d1127a2debe230a49b076f9f5",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "94f7839802c4735b33b2",
+    "url": "/hangman/static/css/main.46225385.chunk.css"
+  },
+  {
+    "revision": "da825246ccdf5d1f7daf",
+    "url": "/hangman/static/js/2.c599a1b6.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/hangman/static/js/2.c599a1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f7839802c4735b33b2",
+    "url": "/hangman/static/js/main.749ff866.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
